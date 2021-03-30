0c24b95b1a6c5a30fbe3059d4bf70d19c79c8427
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-30 14:39:43 +0200

    Styling

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
 
 const Details = () => {
   return (
     <Card>
-      
+      <CardHeader title="Income" />
+      <CardContent>
+        <Typography variant="h5">$50</Typography>      
+        <Doughnut data="DATA" />
+      </CardContent>
     </Card>
   )
 }
