baabab64539df8c1026eac0ea88efa2dd4a01c97
vsreyes <virgilio.s.reyes@gmail.com>
2021-03-31 11:59:10 +0200

    Grid setup

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
 
+import useStyles from './styles';
+
 const Details = () => {
+  const classes = useStyles();
+
   return (
-    <Card>
+    <Card className={classes.income}>
       <CardHeader title="Income" />
       <CardContent>
         <Typography variant="h5">$50</Typography>      
-        <Doughnut data="DATA" />
+        {/*<Doughnut data="DATA" />*/}
       </CardContent>
     </Card>
   )
