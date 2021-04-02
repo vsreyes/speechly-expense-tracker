64f7a557e532615a02166dbd831a12fe182c860b
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-02 16:07:55 +0200

    Form setup

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -32,8 +32,21 @@ const Form = () => {
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        
+        <FormControl fullWidth>
+          <InputLabel>Category</InputLabel>
+          <Select>
+              <MenuItem value="business">Business</MenuItem>
+              <MenuItem value="salary">Salary</MenuItem>
+          </Select>
+        </FormControl>
       </Grid>
+      <Grid item xs={6}>
+        <TextField type="number" label="Amount" fullWidth/>
+      </Grid>
+      <Grid item xs={6}>
+        <TextField type="date" label="Date" fullWidth/>
+      </Grid>
+      <Button className={classes.button} variant="outlined" color="primary" fullWidth>Create</Button>
     </Grid>
   )
 }
