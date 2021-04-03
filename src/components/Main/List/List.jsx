06aad7c30679bd686e9a3b4fec824a957446f8f8
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-03 13:00:39 +0200

    Context setup

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { List as MUIList, ListItem, ListeItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide, ListItemAvatar } from '@material-ui/core'
 import { Delete, MoneyOff } from '@material-ui/icons';
 
+import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from './styles';
 
 const List = () => {
   const classes = useStyles();
 
   const transactions = [
-    {id: 1, type: "Income", category: 'Salary', amount: 50, date: new Date() }
+    {id: 1, type: "Income", category: 'Salary', amount: 50, date: "Sat Apr 16" },
+    {id: 2, type: "Expense", category: 'Pets', amount: 50, date: "Sat Apr 17" },
+    {id: 3, type: "Income", category: 'Business', amount: 50, date: "Sat Apr 18" }
   ];
 
   return (
