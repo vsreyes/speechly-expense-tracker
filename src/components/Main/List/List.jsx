27b9ff198ca564b66ccde206263c9e75e4e91a0d
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-06 15:46:46 +0200

    Categories setup

--- src/components/Main/List/List.jsx
+++ src/components/Main/List/List.jsx
@@ -7,13 +7,7 @@ import useStyles from './styles';
 
 const List = () => {
   const classes = useStyles();
-  const { deleteTransaction } = useContext(ExpenseTrackerContext);
-
-  const transactions = [
-    {id: 1, type: "Income", category: 'Salary', amount: 50, date: "Sat Apr 16" },
-    {id: 2, type: "Expense", category: 'Pets', amount: 50, date: "Sat Apr 17" },
-    {id: 3, type: "Income", category: 'Business', amount: 50, date: "Sat Apr 18" }
-  ];
+  const { deleteTransaction, transactions } = useContext(ExpenseTrackerContext);
 
   return (
     <MUIList dense={false} className={classes.list}>
@@ -27,7 +21,7 @@ const List = () => {
             </ListItemAvatar>
             <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`}/>
               <ListItemSecondaryAction>
-                  <IconButton edge="end" aria-label="delete" onClick="">
+                  <IconButton edge="end" aria-label="delete" onClick={() => deleteTransaction(transaction.id) }>
                     <Delete />
                   </IconButton>
               </ListItemSecondaryAction>
--- src/context/context.js
+++ src/context/context.js
@@ -15,13 +15,12 @@ export const Provider = ({ children }) => {
   const addTransaction = transaction =>
     dispatch({ type: 'ADD_TRANSACTION', payload: transaction });
 
-  console.log(transactions);
-
   return (
     <ExpenseTrackerContext.Provider
       value={{
         deleteTransaction,
         addTransaction,
+        transactions,
       }}
     >
       {children}
