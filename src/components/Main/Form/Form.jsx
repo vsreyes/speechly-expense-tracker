1cbdec86907b3733a00c53e8d7c42e0ab49103fb
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-10 12:11:57 +0200

    Voice record setup

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -29,8 +29,41 @@ const Form = () => {
   }
 
   useEffect(() => {
+      if(segment) {
+        if(segment.intent.intent === 'add_expense') {
+          setFormData({ ...formData, type: 'Expense'});
+        } else if(segment.intent.intent === 'add_income') {
+          setFormData({ ...formData, type: 'Income' });
+        } else if(segment.isFinal && segment.intent.intent === "create_transaction") {
+          return createTransaction();
+        } else if (segment.isFinal && segment.intent.intent === "cancel_transaction") {
+          return setFormData(initialState);
+        }
 
-  }, []);
+        segment.entities.forEach((e) => {
+              const category = `${e.value.charAt(0)}${e.value.slice(1).toLowerCase()}`;
+
+            switch(e.type) {
+              case 'amount':
+                setFormData({ ...formData, amount: e.value })
+                break;
+              case 'category':
+                if(incomeCategories.map((iC) => iC.type).includes(category)) {
+                  setFormData({ ...formData, type: 'Income', category });
+                } else if(expenseCategories.map((iC) => iC.type).includes(category)){
+                  setFormData({ ...formData, type: 'Expense', category });
+                }
+                break;
+              case 'date':
+                setFormData({ ...formData, date: e.value })
+                break;
+              default:
+                break;
+            
+            }
+        })
+      }
+  }, [segment]);
 
   const selectedCategories = formData.type === 'Income' ? incomeCategories : expenseCategories;
 
