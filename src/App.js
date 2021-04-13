df7d3992263f9ebb5f7f28ddf27b3515755b5525
vsreyes <virgilio.s.reyes@gmail.com>
2021-04-13 17:22:01 +0200

    Mobile screen corrections

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Grid } from '@material-ui/core';
 import {
   PushToTalkButton,
   PushToTalkButtonContainer,
   ErrorPanel,
 } from '@speechly/react-ui';
+import { SpeechState, useSpeechContext } from '@speechly/react-client';
 
 import Main from './components/Main/Main';
 import Details from './components/Details/Details';
@@ -12,6 +13,16 @@ import useStyles from './styles';
 
 const App = () => {
   const classes = useStyles();
+  const { speechState } = useSpeechContext();
+  const main = useRef(null);
+
+  const executeScroll = () => main.current.scrollIntoView();
+
+  useEffect(() => {
+    if (speechState === SpeechState.Recording) {
+      executeScroll();
+    }
+  }, [speechState]);
 
   return (
     <div>
@@ -26,7 +37,7 @@ const App = () => {
         <Grid item xs={12} sm={4} className={classes.mobile}>
           <Details title='Income' />
         </Grid>
-        <Grid item xs={12} sm={3} className={classes.main}>
+        <Grid ref={Main} item xs={12} sm={3} className={classes.main}>
           <Main />
         </Grid>
         <Grid item xs={12} sm={4} className={classes.desktop}>
